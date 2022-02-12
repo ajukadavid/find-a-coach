605df93039d62c2cc5e3db9b65ac01d1f6fae9b3
ithiel883 <ajuka.zephiniah@gmail.com>
2022-02-12 12:06:55 +0100

    made some changes

--- src/router.js
+++ src/router.js
@@ -1,16 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import CoachDetail from './pages/coaches/CoachDetail.vue'
+import CoachesList from './pages/coaches/CoachesList.vue'
+import CoachRegistration from './pages/coaches/CoachRegistration.vue'
+import ContactCoach from './pages/requests/ContactCoach.vue'
+import RequestRecieved from './pages/requests/RequestRecieved.vue'
+import NotFound from './pages/NotFound.vue'
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches'},
-        {path: '/coaches', component: null},
-        {path: '/coaches/:id', component:null, children: [
-            {path: 'contact', component: null}
+        {path: '/coaches', component: CoachesList},
+        {path: '/coaches/:id', component:CoachDetail, children: [
+            {path: 'contact', component: ContactCoach}
         ]},
-        {path: '/register'},
-        {path: '/requests', component: null},
-        {path: '/:notFound(.*)', component: nnone}
+        {path: '/register', component: CoachRegistration},
+        {path: '/requests', component: RequestRecieved},
+        {path: '/:notFound(.*)', component: NotFound}
     ]
 })
 
