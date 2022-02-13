e7933579d109f953fa7183cccebd876cb9841190
ithiel883 <ajuka.zephiniah@gmail.com>
2022-02-13 08:16:34 +0100

    implementation of form for coach registration

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,22 @@
 <template>
-  REGISTER
+  <section>
+    <base-card>
+      <h2>Register asa coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../Components/coaches/CoachForm';
+export default {
+  components: {
+    CoachForm
+  }
+}
+
+</script>
+<style scoped>
+
+</style>
